1a6c64fd9a62d8b65585f3df00a1ed103ade181c
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-05-10 16:18:40 +0530

    feat: add interview prep articles

--- front_end/pages/interview-prep.tsx
+++ front_end/pages/interview-prep.tsx
@@ -1,12 +1,26 @@
 import Layout from "../components/Layout";
-import {Container, Title} from "@mantine/core";
+import Head from "next/head";
+import {Container, Paper, Title} from "@mantine/core";
+import Link from "next/link";
+import {INTERVIEW_EXPERIENCES} from "../constants";
 
 export default function InterviewPrep(){
     return (
-        <Layout>
-            <Container>
-                <Title order={2}>Interview Preparation</Title>
-            </Container>
-        </Layout>
+        <>
+            <Head>
+                <title>Interview Preparation</title>
+            </Head>
+            <Layout>
+                <Container>
+                    <Title order={2} mb={"md"}>Interview Preparation</Title>
+                    {INTERVIEW_EXPERIENCES.map((interview, index) => (
+                        <Paper key={index} p={"md"} my={"md"}>
+                            <Title order={3}>{interview.title}</Title>
+                            <p>{interview.body}...<Link style={{color: "blue", textDecoration: "underline"}} href={interview.link} target={"_blank"}>Full article</Link></p>
+                        </Paper>
+                    ))}
+                </Container>
+            </Layout>
+        </>
     )
 }
